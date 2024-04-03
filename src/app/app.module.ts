a8f7356d488b75d54ded23940861c542a8130681
giuliafallati <giulia.fallati@gmx.ch>
2024-04-03 13:27:48 +0200

    modul umgliedern

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,14 @@
 import { NgModule } from '@angular/core';
-import {
-  BrowserModule,
-  provideClientHydration,
-} from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BookListComponent } from './book-list/book-list.component';
-import { BookListItemComponent } from './book-list-item/book-list-item.component';
-import { BookDetailsComponent } from './book-details/book-details.component';
+import { BooksModule } from './books/books.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    BookListComponent,
-    BookListItemComponent,
-    BookDetailsComponent,
-  ],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [provideClientHydration()],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, BooksModule],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
