9368f3d31a56eae7aca9691b5f6aa4cce6c5b563
giuliafallati <giulia.fallati@gmx.ch>
2024-04-03 09:12:23 +0200

    bookmonkey 1st commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { BookListItemComponent } from './book-list-item/book-list-item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BookListComponent,
+    BookListItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
