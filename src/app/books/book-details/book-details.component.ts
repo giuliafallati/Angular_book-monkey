a919cbc34b18ec6dd223033daa82af5040a55509
giuliafallati <giulia.fallati@gmx.ch>
2024-04-04 14:23:52 +0200

    http-schnittstelle, buecher von server und nicht lokal

--- src/app/books/book-details/book-details.component.ts
+++ src/app/books/book-details/book-details.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Book } from '../../shared/book';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { BookStoreService } from '../../shared/book-store.service';
 
 @Component({
@@ -13,8 +13,19 @@ export class BookDetailsComponent {
   constructor(
     private service: BookStoreService,
     private route: ActivatedRoute,
+    private router: Router,
   ) {
     const isbn = this.route.snapshot.paramMap.get('isbn')!;
-    this.book = this.service.getSingle(isbn);
+    this.service.getSingle(isbn).subscribe((book) => {
+      this.book = book;
+    });
+  }
+
+  removeBook(isbn: string) {
+    if (window.confirm('Remove book?')) {
+      this.service.remove(isbn).subscribe(() => {
+        this.router.navigateByUrl('/books');
+      });
+    }
   }
 }
