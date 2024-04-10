45defe73997e0648e69e2a8f701d101903f63792
giuliafallati <giulia.fallati@gmx.ch>
2024-04-10 14:32:50 +0200

    add book erweitert, book-edit erstellt

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
 import { AdminRoutingModule } from './admin-routing.module';
 import { BookFormComponent } from './book-form/book-form.component';
 import { BookCreateComponent } from './book-create/book-create.component';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LocalDateValueAccessor } from 'angular-date-value-accessor';
+import { BookEditComponent } from './book-edit/book-edit.component';
 
 @NgModule({
-  declarations: [BookFormComponent, BookCreateComponent],
-  imports: [CommonModule, AdminRoutingModule, FormsModule],
+  declarations: [BookFormComponent, BookCreateComponent, BookEditComponent],
+  imports: [
+    CommonModule,
+    AdminRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    LocalDateValueAccessor,
+  ],
 })
 export class AdminModule {}
--- src/app/admin/book-form/book-form.component.ts
+++ src/app/admin/book-form/book-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 
 import { Book } from '../../shared/book';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'bm-book-form',
@@ -8,15 +9,43 @@ import { Book } from '../../shared/book';
   styleUrls: ['./book-form.component.css'],
 })
 export class BookFormComponent {
-  book: Book = {
-    isbn: '',
-    title: '',
-    authors: [''],
-  };
-
   @Output() submitBook = new EventEmitter<Book>();
 
+  form = new FormGroup({
+    title: new FormControl('', {
+      nonNullable: true,
+      validators: Validators.required,
+    }),
+    subtitle: new FormControl('', { nonNullable: true }),
+
+    isbn: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(13),
+      ],
+    }),
+    description: new FormControl('', { nonNullable: true }),
+    published: new FormControl('', { nonNullable: true }),
+    thumbnailUrl: new FormControl('', { nonNullable: true }),
+    authors: new FormArray([new FormControl('', { nonNullable: true })]),
+  });
+  get authors() {
+    return this.form.controls.authors;
+  }
+
   submitForm() {
-    this.submitBook.emit(this.book);
+    const formValue = this.form.getRawValue();
+    const authors = formValue.authors.filter((author) => !!author);
+    const newBook: Book = {
+      ...formValue,
+      authors,
+    };
+    this.submitBook.emit(newBook);
+  }
+
+  addAuthorControl() {
+    this.authors.push(new FormControl('', { nonNullable: true }));
   }
 }
