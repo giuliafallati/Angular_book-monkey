9400c4e2c77168fa824be06508d47a9454735df0
giuliafallati <giulia.fallati@gmx.ch>
2024-04-10 08:32:29 +0200

    search fertig, auth erstellt

--- src/app/shared/book-store.service.ts
+++ src/app/shared/book-store.service.ts
@@ -1,25 +1,40 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, catchError, of } from 'rxjs';
 
 import { Book } from './book';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BookStoreService {
-  private books: Book[] = [];
   private apiUrl = 'https://api5.angular-buch.com';
 
   constructor(private http: HttpClient) {}
 
   getAll(): Observable<Book[]> {
-    return this.http.get<Book[]>(`${this.apiUrl}/books`);
+    return this.http.get<Book[]>(`${this.apiUrl}/books`).pipe(
+      catchError((err) => {
+        console.error(err);
+        return of([]);
+      }),
+    );
   }
+
   getSingle(isbn: string): Observable<Book> {
     return this.http.get<Book>(`${this.apiUrl}/books/${isbn}`);
   }
+
   remove(isbn: string): Observable<unknown> {
     return this.http.delete(`${this.apiUrl}/books/${isbn}`);
+  }
+
+  getAllSearch(term: string): Observable<Book[]> {
+    return this.http.get<Book[]>(`${this.apiUrl}/books/search/${term}`).pipe(
+      catchError((err) => {
+        console.error(err);
+        return of([]);
+      }),
+    );
   }
 }
