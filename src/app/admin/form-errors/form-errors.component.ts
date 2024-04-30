8fa8ef45076fab3d6f63cb3c0d96e53cc92bb480
giuliafallati <giulia.fallati@gmx.ch>
2024-04-30 10:48:32 +0200

    validation, alles funktioniert wieder

--- src/app/admin/form-errors/form-errors.component.ts
+++ src/app/admin/form-errors/form-errors.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormGroupDirective } from '@angular/forms';
 
 @Component({
   selector: 'bm-form-errors',
   templateUrl: './form-errors.component.html',
   styleUrl: './form-errors.component.css',
 })
-export class FormErrorsComponent {}
+export class FormErrorsComponent {
+  @Input() controlName?: string;
+  @Input() messages: { [errorCode: string]: string } = {};
+  constructor(private form: FormGroupDirective) {}
+  get errors(): string[] {
+    if (!this.controlName) {
+      return [];
+    }
+
+    const control = this.form.control.get(this.controlName);
+
+    if (!control || !control.errors || !control.touched) {
+      return [];
+    }
+    return Object.keys(control.errors).map((errorCode) => {
+      return this.messages[errorCode];
+    });
+  }
+}
